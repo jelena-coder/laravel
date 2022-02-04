d605fac73ff6dd1b8730e4b997a68320a29aa9ae
Jelena Slijepcevic <jeca.lelica9983@gmail.com>
2022-02-04 22:02:27 +0100

    api funkcija login

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('login', function (Request $request) {
+    $user = User::where('email', $request->email)->first();
+    if (!$user || !Hash::check($request->password, $user->password)) {
+        return response("Failed login", 400);
+    }
+    return $user->createToken($user->id)->plainTextToken;
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', function (Request $request) {
+        $request->user()->currentAccessToken()->delete();
+        return response()->noContent();
+    });
+    Route::apiResource('products', ProductController::class);
 });
